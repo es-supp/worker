cf7a6800f4f5ff9e63bd03c9ae02bece1f030612
es-supp <es-supp@proton.me>
2026-05-06 03:34:41 +0000

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,32 @@
+export default {
+  async fetch(request, env) {
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      });
+    }
+
+    if (request.method === "POST") {
+      const data = await request.json();
+
+      const id = Date.now().toString();
+
+      await env.DB.put(id, JSON.stringify(data));
+
+      console.log("Datos:", data);
+
+      return new Response(JSON.stringify({ ok: true }), {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+    }
+
+    return new Response("Active worker");
+  },
+};
